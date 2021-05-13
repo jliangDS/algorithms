98bc187f1e44fceb6468fe7f01cecf4f84caa243
Jun Liang <illuminatemynight@gmail.com>
2021-05-13 13:33:10 -0500

    modify big-o-approximation

--- big-o-approximation.js
+++ big-o-approximation.js
@@ -1,1 +1,13 @@
-console.log("hello");
+function binarySearch(string, letter) {
+    var startpoint = 0
+    var endpoint = string.length-1;
+    var guessPosition = parseInt((endpoint-startpoint)/2)
+    while (startpoint != endpoint) {
+        console.log(`start point is ${startpoint}, endpoint is ${endpoint} and guessposition is ${guessPosition}`)
+            if (string[guessPosition] < letter) {
+                console.log('too low')
+                    startpoint = guessPosition 
+                    guessPosition = startpoint + Math.round((endpoint - startpoint)/2)
+            }
+    }
+}
